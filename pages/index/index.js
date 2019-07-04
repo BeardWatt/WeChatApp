1928935716457c0cbd13e8fc5de233fa694c3b18
BeardWatt <lizhichensos@126.com>
2019-07-04 13:45:06 +0800

    LZC-Test

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,7 +4,7 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello SB',
+    motto: 'AWSL',
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
